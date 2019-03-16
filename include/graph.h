4980fb1f6375353f6aea8c50851581c5240a8924
Adrien Soursou <26015179+ChuOkupai@users.noreply.github.com>
2019-03-16 15:39:13 +0100

    insertNode issou

--- include/graph.h
+++ include/graph.h
@@ -7,12 +7,12 @@ typedef struct s_Graph Graph;
 struct s_Graph
 {
 	uint8_t	size;
-	Node		**table;
+	Node	**table;
 };
 
 Graph*	newGraph(uint8_t size);
 
-void	insertNode(Graph *g, uint8_t index);
+void	linkNode(Graph *g, uint8_t index1, uint8_t weight1, uint8_t index2, uint8_t weight2);
 
 void	destroyGraph(Graph *g);
 
